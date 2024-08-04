b3f11edb87a577d62e4aa5d1ff5c5ae3ad59687d
em <63263677+subconstant@users.noreply.github.com>
2024-08-04 15:30:06 +0300

    js cleanup + small css fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,63 @@
-const domain = '127.0.0.1'
+const domain = '127.0.0.1' //! change once deployed with domain!
 
-function win(url){
-    /*new WinBox({
-        title: title,
-        url: url
-    });*/
-    let height = window.screen.availHeight-300;
-    let width = window.screen.availWidth-400;
+function win(url) {
+  /*new WinBox({
+      title: title,
+      url: url
+  });*/
+  let height = window.screen.availHeight - 300;
+  let width = window.screen.availWidth - 400;
 
-    window.open(url + '?pop', '_blank', 'toolbar=no,location=no,menubar=no,top=150,left=200,width=' + width + ',height=' + height);
+  window.open(url + '?pop', '_blank', 'toolbar=no,location=no,menubar=no,top=150,left=200,width=' + width + ',height=' + height);
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#backlink').attr('href', document.referrer);
+  $('#backlink').attr('href', document.referrer);
 
-    // Below IFs configure visibility of logo and back button 
-    if(window.menubar.visible){
-        let url = new URL(location);
-        url.searchParams.delete('pop');
-        history.replaceState(null, null, url)
+  // Below IFs configure visibility of logo and back button 
+  if (window.menubar.visible) {
+    let url = new URL(location);
+    url.searchParams.delete('pop');
+    history.replaceState(null, null, url)
+  }
+
+  if (window.location.search.includes('?pop')) {
+    document.getElementById('postlogo').style.display = 'none';
+  }
+
+  if (window.location.search.includes('?sub')) {
+
+    if (document.referrer.includes(domain)) {
+      document.getElementById('backlink').style.display = 'inline';
     }
 
-    if(window.location.search.includes('?pop')){
-        document.getElementById('postlogo').style.display = 'none';
+    if (document.referrer.includes('?pop')) {
+      document.getElementById('postlogo').style.display = 'none';
     }
 
-    if(window.location.search.includes('?sub')){
+  }
 
-        if(document.referrer.includes(domain)){
-            document.getElementById('backlink').style.display = 'inline';   
-        }
-
-        if(document.referrer.includes('?pop')){
-            document.getElementById('postlogo').style.display = 'none';
-        }
-
-    }
-
-
-    $(".postcontent img").each(function(index, element) {
-        $(element).wrap("<div class='postimg'></div>");
-    });
-
+  $(".postcontent img").each(function (index, element) {
+    $(element).wrap("<div class='postimg'></div>");
+  });
 });
 
 document.addEventListener('mousemove', preview, false);
 
-function preview() {
-
-    for (var i=$('.postpreview').length; i--;) {
-        $('.postpreview')[i].style.left = event.clientX + 'px';
-        $('.postpreview')[i].style.top = event.clientY + 'px';
-    }
-
+function preview(e) {
+  for (var i = $('.postpreview').length; i--;) {
+    $('.postpreview')[i].style.left = e.clientX + 'px';
+    $('.postpreview')[i].style.top = e.clientY + 'px';
+  }
 }
 
-window.onscroll = function() {
-var appear = 20
-if (window.pageYOffset >= appear) {
-  document.getElementById('postinfo').style.opacity = '0'
-  document.getElementById('postinfo').style.pointerEvents = 'none'
-} else {
-  document.getElementById('postinfo').style.opacity = '1'
-  document.getElementById('postinfo').style.pointerEvents = 'all'
-  
-}
+window.onscroll = function () {
+  if (window.scrollY >= 20) {
+    document.getElementById('postinfo').style.opacity = '0'
+    document.getElementById('postinfo').style.pointerEvents = 'none'
+  } else {
+    document.getElementById('postinfo').style.opacity = '1'
+    document.getElementById('postinfo').style.pointerEvents = 'all'
+  }
 }
